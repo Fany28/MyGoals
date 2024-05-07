928d452a049471e0e5009227ed96a5612d19dda4
Fany28 <cortezgonzalezlucero@gmail.com>
2024-05-07 15:29:52 -0700

    Avance MyGoals

--- App.js
+++ App.js
@@ -1,10 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react'
+import { StyleSheet,View, FlatList } from 'react-native';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
+
 
 export default function App() {
+  const [goals, setGoals] =useState([])
+
+
+  function handleAddGoal(enteredGoalText) {
+    //console.log(enteredGoalText)
+    //console.log('Hello you')
+    setGoals(() => [...goals, {text: enteredGoalText, key: Math.random(). toString()}])
+    console.log(goals)
+  }
+
+  function handleDeleteGoal(){
+    console.log('DELETE')
+  }
+
+
   return (
     <View style={styles.container}>
-      
+
+    <GoalInput
+    onAddGoal={handleAddGoal}
+    />
+    <View style={styles.goalsConatiner}>
+      <FlatList
+      data={goals}
+      renderItem={ (itemData) => {
+        return(
+          <GoalItem
+          itemData={itemData}
+          onDeleteItem={handleDeleteGoal}
+          />
+        )
+      }}
+      />
+    </View>
     </View>
   );
 }
@@ -12,8 +46,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 20,
+  },
+  goalsConatiner: {
+    flex: 5
   },
 });
